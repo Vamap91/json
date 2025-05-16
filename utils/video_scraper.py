fe199513aaa78782bf7a85ccf72f2ad4a6e18fd6
Vamap91 <viniciusaugustopaschoa1@gmail.com>
2025-05-16 08:53:43 -0300

    Update video_scraper.py

--- utils/video_scraper.py
+++ utils/video_scraper.py
@@ -1,22 +1,26 @@
 import requests
-from bs4 import BeautifulSoup
 import pandas as pd
 
 def extract_videos(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    video_elements = soup.find_all('div', class_='video-container')
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        videos_json = response.json()
+    except Exception as e:
+        print(f"Erro ao acessar ou processar o JSON: {e}")
+        return pd.DataFrame()
 
     df_data = []
-    for video in video_elements:
+
+    for video in videos_json:
         df_data.append({
-            'ID': video.get('data-id'),
-            'Source': url,
-            'Title': video.find('h3').text if video.find('h3') else '',
-            'Description': video.find('p').text if video.find('p') else '',
-            'Video URL': video.find('a')['href'] if video.find('a') else '',
-            'Thumbnail URL': video.find('img')['src'] if video.find('img') else '',
-            'Language': 'pt-BR'
+            'ID': video.get('id', ''),
+            'Source': video.get('source', ''),
+            'Title': video.get('title', ''),
+            'Description': video.get('description', ''),
+            'Video URL': video.get('video_url', ''),
+            'Thumbnail URL': video.get('thumbnail_url', ''),
+            'Language': video.get('language', '')
         })
 
     return pd.DataFrame(df_data)
